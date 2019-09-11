b14672d32c511170b55491eb1aec71a24d5b5a08
unknown <dreamcodesoft@gmail.com>
2019-09-12 00:58:07 +0300

    Menuflow and Template

--- Assets/GameManagerControler.cs
+++ Assets/GameManagerControler.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class GameManagerControler : MonoBehaviour
 {
     public GameStateHandler gameStateHandler;
 
